2fbf43f73a996cbb70da08a2efcf82acc1af5cbc
ToastXC <100072983+toastxc@users.noreply.github.com>
2024-03-24 13:28:36 +0800

    improved widget creator, made 'error block'

--- src/lib.rs
+++ src/lib.rs
@@ -63,76 +63,81 @@ fn c(i: Argb) -> Color32 {
 }
 
 impl MaterialColors {
-
     pub fn new(base_color: String, dark: bool, zoom: f32) -> Self {
-        let mut new = Self::default();
-        new.base_color = base_color;
-        new.dark = dark;
-        new.zoom = zoom;
-        new
+        let scheme = material_colors::theme_from_source_color(
+            Argb::from_str(&base_color).unwrap(),
+            Default::default(),
+        )
+        .schemes;
+        let scheme = match dark {
+            true => scheme.dark,
+            false => scheme.light,
+        };
+        Self {
+            base_color,
+            dark,
+            zoom,
+            // primary
+            primary: c(scheme.primary),
+            on_primary: c(scheme.on_primary),
+            primary_container: c(scheme.primary_container),
+            on_primary_container: c(scheme.on_primary_container),
+            inverse_primary: c(scheme.inverse_primary),
+            primary_fixed: c(scheme.primary_fixed),
+            primary_fixed_dim: c(scheme.primary_fixed_dim),
+            on_primary_fixed: c(scheme.on_primary_fixed),
+            on_primary_fixed_variant: c(scheme.on_primary_fixed_variant),
+            // secondary
+            secondary: c(scheme.secondary),
+            on_secondary: c(scheme.on_secondary),
+            secondary_container: c(scheme.secondary_container),
+            on_secondary_container: c(scheme.on_secondary_container),
+            secondary_fixed: c(scheme.secondary_fixed),
+            secondary_fixed_dim: c(scheme.secondary_fixed_dim),
+            on_secondary_fixed: c(scheme.on_secondary_fixed),
+            on_secondary_fixed_variant: c(scheme.on_secondary_fixed_variant),
+            // tertiary
+            tertiary: c(scheme.tertiary),
+            on_tertiary: c(scheme.on_tertiary),
+            tertiary_container: c(scheme.tertiary_container),
+            on_tertiary_container: c(scheme.on_tertiary_container),
+            tertiary_fixed: c(scheme.tertiary_fixed),
+            tertiary_fixed_dim: c(scheme.tertiary_fixed_dim),
+            on_tertiary_fixed: c(scheme.on_tertiary_fixed),
+            on_tertiary_fixed_variant: c(scheme.on_tertiary_fixed_variant),
+            // error
+            error: c(scheme.error),
+            on_error: c(scheme.on_error),
+            error_container: c(scheme.error_container),
+            on_error_container: c(scheme.on_error_container),
+            // surface
+            surface_dim: c(scheme.surface_dim),
+            surface: c(scheme.surface),
+            surface_bright: c(scheme.surface_bright),
+            surface_container_lowest: c(scheme.surface_container_lowest),
+            surface_container_low: c(scheme.surface_container_low),
+            surface_container: c(scheme.surface_container),
+            surface_container_high: c(scheme.surface_container_high),
+            surface_container_highest: c(scheme.surface_container_highest),
+            on_surface: c(scheme.on_surface),
+            on_surface_variant: c(scheme.on_surface_variant),
+            // outline
+            outline: c(scheme.outline),
+            outline_variant: c(scheme.outline_variant),
+            // inverse
+            inverse_surface: c(scheme.inverse_surface),
+            inverse_on_surface: c(scheme.inverse_on_surface),
+            surface_variant: c(scheme.surface_variant),
+            background: c(scheme.background),
+            on_background: c(scheme.on_background),
+            shadow: c(scheme.shadow),
+            scrim: c(scheme.scrim),
+        }
     }
     /// exports the default M3 theme
     /// this function applies M3 themes to egui assets, specific implementation is subject to change
     /// CANNOT apply zoom
     pub fn export(&self) -> Style {
-        let p = self.from_values();
-
-        let visuals = Visuals {
-            override_text_color: Some(p.primary),
-            hyperlink_color: p.on_primary,
-            // background
-            faint_bg_color: p.surface_container,
-            extreme_bg_color: p.surface_variant,
-            code_bg_color: p.surface_dim,
-
-            window_fill: p.surface_container_highest,
-            panel_fill: p.surface,
-
-            warn_fg_color: p.error_container,
-            error_fg_color: p.error,
-
-            window_stroke: egui::Stroke {
-                color: p.secondary,
-                ..Default::default()
-            },
-            window_highlight_topmost: false,
-            widgets: widgets(p.clone()),
-            window_shadow: epaint::Shadow {
-                color: p.shadow,
-
-                ..Default::default()
-            },
-
-            popup_shadow: epaint::Shadow {
-                color: p.shadow,
-                ..Default::default()
-            },
-
-            selection: Selection {
-                bg_fill: p.tertiary,
-                stroke: Default::default(),
-            },
-            ..Default::default()
-        };
-
-        Style {
-            visuals,
-            ..Default::default()
-        }
-    }
-    /// applies generated values to ctx
-    pub fn apply(&self, ctx: &Context) {
-        ctx.set_style(self.export());
-        ctx.set_zoom_factor(self.zoom);
-    }
-    /// applies generated values to Ui
-    /// CANNOT apply zoom
-    pub fn apply_ui(&self, ui: &mut Ui) {
-        ui.set_style(self.export())
-    }
-
-    /// creates color palette
-    pub fn from_values(&self) -> Self {
         let base_color = self.base_color.clone();
         let scheme = material_colors::theme_from_source_color(
             Argb::from_str(&base_color).unwrap(),
@@ -143,8 +148,7 @@ impl MaterialColors {
             true => scheme.dark,
             false => scheme.light,
         };
-
-        Self {
+        let p = Self {
             base_color,
             dark: self.dark,
             zoom: self.zoom,
@@ -203,29 +207,101 @@ impl MaterialColors {
             on_background: c(scheme.on_background),
             shadow: c(scheme.shadow),
             scrim: c(scheme.scrim),
+        };
+
+        let visuals = Visuals {
+            // override_text_color: Some(p.primary),
+            collapsing_header_frame: true,
+            override_text_color: None,
+            hyperlink_color: p.on_primary,
+            // background
+            faint_bg_color: p.surface_container,
+            extreme_bg_color: p.surface_variant,
+            code_bg_color: p.surface_dim,
+
+            window_fill: p.surface_container_highest,
+            panel_fill: p.surface,
+
+            warn_fg_color: p.error_container,
+            error_fg_color: p.error,
+
+            window_stroke: egui::Stroke {
+                color: p.secondary,
+                ..Default::default()
+            },
+            window_highlight_topmost: false,
+            widgets: widgets(p.clone()),
+            window_shadow: epaint::Shadow {
+                color: p.shadow,
+
+                ..Default::default()
+            },
+
+            popup_shadow: epaint::Shadow {
+                color: p.shadow,
+                ..Default::default()
+            },
+
+            selection: Selection {
+                bg_fill: p.tertiary,
+                stroke: Default::default(),
+            },
+            ..Default::default()
+        };
+
+        Style {
+            visuals,
+            ..Default::default()
         }
     }
-}
+    /// applies generated values to ctx
+    pub fn apply(&self, ctx: &Context) {
+        ctx.set_style(self.export());
+        ctx.set_zoom_factor(self.zoom);
+    }
+    /// applies generated values to Ui
+    /// CANNOT apply zoom
+    pub fn apply_ui(&self, ui: &mut Ui) {
+        ui.set_style(self.export())
+    }
 
-
-// widget helping tools
-fn widgets(p: MaterialColors) -> Widgets {
-    let old = Visuals::dark().widgets;
-
-    Widgets {
-        noninteractive: widget_maker(old.noninteractive, p.surface, p.surface_container),
-        inactive: widget_maker(old.inactive, p.primary_container, p.on_primary_container),
-        hovered: widget_maker(old.hovered, p.tertiary_container, p.on_tertiary_container),
-        active: widget_maker(old.active, p.inverse_primary, p.on_primary_container),
-        open: widget_maker(old.open, p.primary_container, p.on_primary_container),
+    pub fn error_apply(&self, ui: &mut Ui) {
+        let widgets = &mut ui.style_mut().visuals.widgets;
+        // 'error buttons' are not documented in M3, so its unclear which arrangement is better
+        let active = (self.error, self.on_error);
+        let rest = (self.error_container, self.on_error_container);
+        // widgets
+        widget_maker_mut(&mut widgets.hovered, rest.0, rest.1);
+        widget_maker_mut(&mut widgets.inactive, rest.0, rest.1);
+        widget_maker_mut(&mut widgets.open, active.0, active.1);
+        widget_maker_mut(&mut widgets.active, active.0, active.1);
     }
 }
+fn widgets(p: MaterialColors) -> Widgets {
+    let mut old = match p.dark {
+        true => Visuals::dark(),
+        false => Visuals::light(),
+    }
+    .widgets;
+    widget_maker_mut(&mut old.noninteractive, p.surface, p.on_surface);
+    widget_maker_mut(
+        &mut old.inactive,
+        p.primary_container,
+        p.on_primary_container,
+    );
+    widget_maker_mut(
+        &mut old.hovered,
+        p.tertiary_container,
+        p.on_tertiary_container,
+    );
+    widget_maker_mut(&mut old.active, p.inverse_primary, p.on_primary_container);
+    widget_maker_mut(&mut old.open, p.primary_container, p.on_primary_container);
 
-fn widget_maker(old: WidgetVisuals, c1: Color32, c2: Color32) -> WidgetVisuals {
-    let mut old = old;
-    old.bg_fill = c1;
-    old.weak_bg_fill = c1;
-    old.fg_stroke.color = c2;
-    old.bg_stroke.color = c2;
     old
+}
+fn widget_maker_mut(old: &mut WidgetVisuals, fill: Color32, text: Color32) {
+    old.bg_fill = fill;
+    old.weak_bg_fill = fill;
+    old.fg_stroke.color = text;
+    old.bg_stroke.color = fill;
 }
